dc29f73cfb72f01956d70670b1daa78e8d6e18d8
kimdonghyun12345 <gdm200180@gmail.com>
2026-04-17 14:25:52 +0900

    Initial Commit

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -5,10 +5,22 @@ class Rabbit {
 
     void setPosition(int x, int y) {
         xPos = x;
-        yPos = y;
+        xPos = y;
+
+        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit();
+        Rabbit rabbit3 = new Rabbit();
+
+        rabbit1.shape = "원";
+        rabbit2.shape = "삼각형";
+        rabbit3.shape = "토끼";
+
+        rabbit1.setPosition(100, 100);
+        rabbit2.setPosition(-100, 100);
+        rabbit3.setPosition(0, 100);
+
     }
 }
-
 public class Code07_06 {
     public static void main(String[] args) {
 
