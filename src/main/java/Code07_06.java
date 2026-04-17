3383240df9803fa668972bd3efc9d7b7877a51c4
kimdonghyun12345 <gdm200180@gmail.com>
2026-04-17 15:23:15 +0900

    Initial Commit

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -1,27 +1,20 @@
 class Rabbit {
-    String shape;
-    int xPos;
-    int yPos;
 
-    Rabbit(String value) {
-        shape = value;
-    }
+     public String shape;
+     public int xPos;
+     public int yPos;
 
     void setPosition(int x, int y) {
         xPos = x;
         xPos = y;
     }
 }
+
 public class Code07_06 {
     public static void main(String[] args) {
-        Rabbit rabbit1 = new Rabbit("원");
-        Rabbit rabbit2 = new Rabbit("삼각형");
-        Rabbit rabbit3 = new Rabbit("토끼");
+        Rabbit rabbit = new Rabbit();
 
-
-        System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.\n", rabbit1.shape);
-        System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.\n", rabbit2.shape);
-        System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.\n", rabbit3.shape);
-
+        rabbit.xPos = 100;
+        rabbit.yPos = 200;
     }
 }
